d4fb4c2788081138367aadbe8b29964fd782a0cb
Kasper Skytte Andersen <knaldhat@gmail.com>
2021-08-16 16:20:38 +0200

    chmod 644

--- ampvis2tricks/searchTaxa.R
+++
@@ -1,9 +0,0 @@
-library(ampvis2)
-searchStrings <- c("nocardioides", "trichococcus", "carnobacter")
-d <- AalborgWWTPs
-
-result <- unlist(lapply(d$tax, function(x) {
-  unique(x[stringi::stri_detect_regex(str = tolower(x), pattern = paste0(tolower(searchStrings), collapse = "|"))])
-}), use.names = FALSE)
-
-ds <- amp_subset_taxa(d, result)
