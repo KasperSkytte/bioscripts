3e7cfcc09216342a68b1586ffa9c6c26dbaf466c
Kasper Skytte Andersen <knaldhat@gmail.com>
2022-03-16 12:36:44 +0100

    convert to function

--- addsintaxtofasta.R
+++ addsintaxtofasta.R
@@ -1,34 +1,24 @@
-  suppressPackageStartupMessages({
-    #Biostrings (and BiocManager which is used to install Biostrings)
-    if(!require("Biostrings")) {
-      if(!require("BiocManager")) {
-        install.packages("BiocManager")
-      }
-      BiocManager::install("Biostrings", update = FALSE, ask = FALSE)
-    }
-    if(!require("Biostrings"))
-      stop("   The Biostrings R package is not available, skipping step", call. = FALSE)
-    
-    #dplyr
-    if(!require("dplyr")) {
-      install.packages("dplyr")
-      require("dplyr")
-    }
-    
-    #plyr
-    if(!require("plyr")) {
-      install.packages("plyr")
-      require("plyr")
-    }
-    
-    #stringr
-    if(!require("stringr")) {
-      install.packages("stringr")
-      require("stringr")
-    }
-  })
+#!/usr/bin/env R
+addsintaxtofasta <- function(
+  input_seqs,
+  sintax_file,
+  output_seqs = paste0(
+    tools::file_path_sans_ext(input_seqs),
+    "_w_sintax.",
+    tools::file_ext(input_seqs)
+  )
+) {
+  req_pkgs <- c("Biostrings", "dplyr", "plyr", "stringr")
+  pkg_status <- lapply(req_pkgs, require, character.only = TRUE)
+  if (!all(unlist(pkg_status))) {
+    stop(
+      "The following packages are required, please install manually:\n",
+      paste(req_pkgs, collapse = "\n"), call. = FALSE
+    )
+  }
+
   ##### export ASVs with SINTAX taxonomy in headers #####
-  sintax <- readLines("ASVsV13_v1.0_20190205/ASVs.R1.sintax")
+  sintax <- readLines(sintax_file)
   taxdf <- plyr::ldply(str_split(sintax, "\t"), function(x) {
     x <- x[c(1,4)]
     if(is.null(x[2]) | x[2] == "")
@@ -36,9 +26,10 @@
     return(x)
   })
   colnames(taxdf) <- c("ASV", "tax")
-  
-  ASVs.fa <- readBStringSet("ASVsV13_v1.0_20190205/ASVs.R1.fa")
+
+  ASVs.fa <- readBStringSet(input_seqs)
   sintaxdf <- left_join(tibble(ASV = names(ASVs.fa)), taxdf, by = "ASV")
   sintax_header <- paste0(sintaxdf[["ASV"]], ";tax=", sintaxdf[["tax"]], ";")
   names(ASVs.fa) <- sintax_header
-  writeXStringSet(ASVs.fa, "ASVs.R1.w_sintax.fa")
+  writeXStringSet(ASVs.fa, output_seqs)
+}
